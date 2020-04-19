fa3161fd55fc4c8a0a4682de47fa88221c9424a7
dabrowskia <szaman.adam@gmail.com>
2020-04-19 16:35:47 +0200

    new version

--- otodom_ab/R/draft_scripts.R
+++
@@ -1,8 +0,0 @@
-
-# transform data ----------------------------------------------------------
-
-
-ab_data <- read.table('otodom_ab/data/ab_data.csv', sep = ',', header = T)
-pp_user <- read.table('otodom_ab/data/pp_users.csv', sep = ',', header = T)
-
-save(ab_data, pp_user, file = 'otodom_ab/data/input_data.RData')
